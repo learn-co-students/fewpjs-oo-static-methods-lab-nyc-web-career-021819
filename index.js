1fb9f06a2784daae1d45471354b7c239980ddf90
tchang9 <tlchang@gmail.com>
2019-04-10 17:44:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'-\s]+/g, '')
+  }
+  
+  static titleize(string) {
+    const sentenceArray = string.split(" ")
+    const badWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    Formatter.capitalize(sentenceArray[0])
+    const newSent = sentenceArray.map(word => {
+      if (!badWords.includes(word)) {
+        return Formatter.capitalize(word)
+      } else {
+        return word
+      }
+    }).join(' ')
+    return Formatter.capitalize(newSent[0]) + newSent.slice(1)
+  }
 }

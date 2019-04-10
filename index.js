dcf9978a09264e3c834b110c33fa36aa4c75cc44
adamjgordon28 <adamjgordon24@gmail.com>
2019-04-10 17:21:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+   let firstLetter = string.charAt(0).toUpperCase()
+   return (firstLetter + string.slice(1))
+  }
+  static sanitize(string) {
+   let sanitizedString = string.replace(/[`~!@#$%^&*()_|+\=?;:",.<>\{\}\[\]\\\/]/gi, '');
+   return sanitizedString
+  }
+  static titleize(string) {
+   let array = string.split(" ")
+   let firstWord = Formatter.capitalize(array[0])
+   const exceptionsArray = ["the","a","an" ,"but", "of", "and" , "for" , "at" ,"by","from"]
+
+  let mapArray = array.slice(1).map(word => {
+    if (exceptionsArray.includes(word)){
+      return word
+    }
+    else {
+       return Formatter.capitalize(word)
+    }
+  })
+  let newArray = firstWord + " " + mapArray.join(" ")
+  return newArray
+  }
+
 }

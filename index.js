dca97b2a1df7ab784109f4f103a264753fbb6a67
dlee16 <diana.lee116@gmail.com>
2019-04-11 19:09:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  static titleize(string){
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let strArray = string.split(' ').map(word => {
+      if (exceptions.includes(word)){
+        return word
+      } else {
+        return this.capitalize(word)
+      }
+    }).join(' ');
+    return this.capitalize(strArray)
+  }
 }

85f251fcfcafa412c98a1684d70d5cc9e7b1c45d
David Korff <dkorff12@gmail.com>
2019-04-10 20:46:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]/g, '');
+  }
+
+  static titleize(string) {
+    let stringArr = string.split(' ')
+    let excludedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return stringArr.map( (word, index) => {
+      if (excludedWords.includes(word) && index !== 0) {
+        return word
+      } else {
+        return word.charAt(0).toUpperCase() + word.slice(1)
+      }
+    }).join(' ')
+  }
+
+
 }

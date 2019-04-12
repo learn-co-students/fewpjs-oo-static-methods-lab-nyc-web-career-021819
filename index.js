bf187b3529ed793c8a7c2453d0488d3f0de42a60
Jessy Hu <jesshuy12@gmail.com>
2019-04-11 22:21:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'-\s]+/g, '')
+  }
+
+  static titleize(string) {
+    const sentence = string.split(" ")
+    const discard = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    Formatter.capitalize(sentence[0])
+    const newSentence = sentence.map(function(word) {
+      if (!discard.includes(word)) {
+        return Formatter.capitalize(word)
+      } else {
+        return word
+      }
+    }).join(' ')
+    return Formatter.capitalize(newSentence[0]) + newSentence.slice(1)
+  }
+
+}//end of class Formatter
